c212628d9406af25d12aab3742f63e0e9315553e
Bafometz <pro100swt@gmail.com>
2023-08-17 22:06:51 +0300

    Почуть привел код в порядок, добавил вывод ошибок в cerr

--- src/interfaceInformation/include/ifaceinformation.h
+++ src/interfaceInformation/include/ifaceinformation.h
@@ -1,128 +1,40 @@
 #ifndef NETWORKINFORMER_H
 #define NETWORKINFORMER_H
+#include "struct_definition.h"
 #include "version.h"
 #include <bitset>
-#include <iostream>
-#include <memory>
-#include <string>
-#include <vector>
+#include <map>
+
 struct ifreq;
 
 namespace iface_helper
 {
-    enum class ip_version
-    {
-        empty = -1,
-        ip_v4 = 0,
-        ip_v6
-    };
-
-    struct Ip_s
-    {
-        std::string ip { "" };  ///< Человекочитаемый ip
-        u_int64_t   ipInt { 0 };
-        ip_version  version { ip_version::empty };
-
-        bool isV4() { return version == ip_version::ip_v4; };
-
-        bool isV6() { return version == ip_version::ip_v6; };
-
-        std::string versionStr()
-        {
-            switch (version)
-            {
-            case iface_helper::ip_version::empty:
-                return "";
-            case iface_helper::ip_version::ip_v4:
-                return "IpV4";
-            case iface_helper::ip_version::ip_v6:
-                return "IpV6";
-            }
-        };
-    };
-
-    using IpPtr = std::shared_ptr< Ip_s >;
-
-    /**
-     *  @brief Структура интерфейса, включает в себя
-     *  информацию об одном из Интернет интерфейсов
-     */
-    struct NetInfo
-    {
-        std::string          interface = "";      ///< Имя интерфейса
-        std::vector< IpPtr > ip;                  ///< Список ip адресов
-        std::string          netMask    = "";     ///< Маска интерфейса
-        std::string          broadcast  = "";     ///< Широковещательный адрес
-        std::string          network    = "";     ///< Сеть в которой находится адрес
-        std::string          macAddress = "";     ///< Мак адрес
-        int                  mtu        = 0;      ///< Значение МТУ
-        bool                 isUp       = false;  ///< Поднят интерфейс или нет
-        bool                 isLoopBack = false;  ///< Виртуальный адрес
-
-        friend std::ostream& operator<<(std::ostream& os, const NetInfo& ni)
-        {
-            os << "Interface: " << ni.interface << "\n"
-               << "Net mask: " << ni.netMask << "\n"
-               << "BroadCast address: " << ni.broadcast << "\n"
-               << "Network: " << ni.network << "\n"
-               << "Is interface up: " << (ni.isUp ? "true" : "false") << "\n"
-               << "Is interface LoopBack: " << (ni.isLoopBack ? "true" : "false") << "\n";
-            for (const auto& element : ni.ip)
-            {
-                os << "Ip: " << element->ip << " version " << element->versionStr() << "\n";
-            }
-            os << "\n";
-            return os;
-        }
-    };
-
-    struct NetInfoNew
-    {
-        std::string          interface = "";      ///< Имя интерфейса
-        std::vector< IpPtr > ip;                  ///< Список ip адресов
-        std::string          netMask    = "";     ///< Маска интерфейса
-        std::string          broadcast  = "";     ///< Широковещательный адрес
-        std::string          network    = "";     ///< Сеть в которой находится адрес
-        std::string          macAddress = "";     ///< Мак адрес
-        int                  mtu        = 0;      ///< Значение МТУ
-        bool                 isUp       = false;  ///< Поднят интерфейс или нет
-        bool                 isLoopBack = false;  ///< Виртуальный адрес
-
-        friend std::ostream& operator<<(std::ostream& os, const NetInfo& ni)
-        {
-            os << "Interface: " << ni.interface << "\n"
-               << "Net mask: " << ni.netMask << "\n"
-               << "BroadCast address: " << ni.broadcast << "\n"
-               << "Network: " << ni.network << "\n"
-               << "Is interface up: " << (ni.isUp ? "true" : "false") << "\n"
-               << "Is interface LoopBack: " << (ni.isLoopBack ? "true" : "false") << "\n";
-            for (const auto& element : ni.ip)
-            {
-                os << "Ip: " << element->ip << " version " << element->versionStr() << "\n";
-            }
-            os << "\n";
-            return os;
-        }
-    };
-
-    using NetInfoPtr     = std::shared_ptr< NetInfo >;
-    using InterfacesList = std::vector< NetInfoPtr >;
-
     class UnixNetworkInformer;
 
     class NetworkInformer
     {
       public:
-        NetworkInformer();
-        InterfacesList getInterfacesList();                             ///< Возвращает список интерфейсов
-        NetInfoPtr     getInterfaceInfo(const std::string& ifaceName);  ///< Возвращает конкретный интерфейс
-        void           update();                                        ///< Обновляет информацию
+        NetworkInformer() noexcept;
+        InterfacesList getInterfacesList() noexcept;                             ///< Возвращает список интерфейсов
+        NetInfoPtr     getInterfaceInfo(const std::string& ifaceName) noexcept;  ///< Возвращает конкретный интерфейс
+        void           update() noexcept;                                        ///< Обновляет информацию
         ~NetworkInformer();
-    private:
-        NetInfoPtr parseReq(const ifreq &ifr, int sd);
 
       private:
-        std::unique_ptr< UnixNetworkInformer > m_networkInformer;
+        NetInfoPtr parseReq(const ifreq& ifr, int sd) noexcept;
+
+        void        update_information();
+        bool        update_ip(int sd, ifreq* ifr, NetInfoPtr);
+        bool        update_broadcast(int sd, ifreq* ifr, NetInfoPtr ni);
+        bool        update_mac(int sd, ifreq* ifr, NetInfoPtr ni);
+        bool        update_net_mask(int sd, ifreq* ifr, NetInfoPtr ni);
+        bool        update_mtu(int sd, ifreq* ifr, NetInfoPtr ni);
+        bool        update_network(int sd, ifreq* ifr, NetInfoPtr ni);
+
+        std::string ipToStr(int ip);
+
+      private:
+        std::map< std::string, NetInfoPtr > m_information;
     };
 
 }  // namespace iface_helper
